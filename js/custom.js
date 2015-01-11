2cbb6a0e1e963806c035c55f8f1d18a7b712c44c
Rob Prouse <rob@prouse.org>
2015-01-11 10:39:19 -0500

    Commented out unused javascript

--- js/custom.js
+++ js/custom.js
@@ -168,21 +168,21 @@ $(window).resize(function() {
     /*	SCREENSHOT CAROUSEL
 	/*---------------------------------------*/
 
-    $("#screenshots").owlCarousel({
-        navigation: false,
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true
-    });
+    //$("#screenshots").owlCarousel({
+    //    navigation: false,
+    //    slideSpeed: 300,
+    //    paginationSpeed: 400,
+    //    singleItem: true
+    //});
 
 
     /*---------------------------------------*/
     /*	SCREENSHOT LIGHTBOX
 	/*---------------------------------------*/
 
-    $('#screenshots a').nivoLightbox({
-        effect: 'fadeScale',
-    });
+    //$('#screenshots a').nivoLightbox({
+    //    effect: 'fadeScale',
+    //});
 
 
     /*---------------------------------------*/
@@ -203,7 +203,7 @@ $(window).resize(function() {
         }
     }).blur();
 
-    //ENSURE PLACEHOLDER TEEXT IS NOT SUBMITTED AS POST
+    //ENSURE PLACEHOLDER TEXT IS NOT SUBMITTED AS POST
     $('[placeholder]').parents('form').submit(function() {
         $(this).find('[placeholder]').each(function() {
             var input = $(this);
@@ -248,117 +248,117 @@ $(window).resize(function() {
 /*---------------------------------------*/
 /*	GOOGLE MAP
 /*---------------------------------------*/
-jQuery(document).ready(function($) {
+//jQuery(document).ready(function($) {
 
-    "use strict";
-    //set your google maps parameters
-    var $latitude = 51.522532, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
-        $longitude = 0.031639,
-        $map_zoom = 16; /* ZOOM SETTING */
+//    "use strict";
+//    //set your google maps parameters
+//    var $latitude = 51.522532, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
+//        $longitude = 0.031639,
+//        $map_zoom = 16; /* ZOOM SETTING */
 
-    //google map custom marker icon - .png fallback for IE11
-    var is_internetExplorer11 = navigator.userAgent.toLowerCase().indexOf('trident') > -1;
-    var $marker_url = (is_internetExplorer11) ? 'images/map-marker.png' : 'images/map-marker.svg';
+//    //google map custom marker icon - .png fallback for IE11
+//    var is_internetExplorer11 = navigator.userAgent.toLowerCase().indexOf('trident') > -1;
+//    var $marker_url = (is_internetExplorer11) ? 'images/map-marker.png' : 'images/map-marker.svg';
 
-    //we define here the style of the map
-    var style = [{
-        "stylers": [{
-            "hue": "#00aaff"
-        }, {
-            "saturation": -100
-        }, {
-            "gamma": 2.15
-        }, {
-            "lightness": 12
-        }]
-    }];
+//    //we define here the style of the map
+//    var style = [{
+//        "stylers": [{
+//            "hue": "#00aaff"
+//        }, {
+//            "saturation": -100
+//        }, {
+//            "gamma": 2.15
+//        }, {
+//            "lightness": 12
+//        }]
+//    }];
 
-    //set google map options
-    var map_options = {
-        center: new google.maps.LatLng($latitude, $longitude),
-        zoom: $map_zoom,
-        panControl: true,
-        zoomControl: true,
-        mapTypeControl: false,
-        streetViewControl: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scrollwheel: false,
-        styles: style,
-    }
-    //inizialize the map
-    var map = new google.maps.Map(document.getElementById('google-container'), map_options);
-    //add a custom marker to the map				
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng($latitude, $longitude),
-        map: map,
-        visible: true,
-        icon: $marker_url,
-    });
+//    //set google map options
+//    var map_options = {
+//        center: new google.maps.LatLng($latitude, $longitude),
+//        zoom: $map_zoom,
+//        panControl: true,
+//        zoomControl: true,
+//        mapTypeControl: false,
+//        streetViewControl: true,
+//        mapTypeId: google.maps.MapTypeId.ROADMAP,
+//        scrollwheel: false,
+//        styles: style,
+//    }
+//    //inizialize the map
+//    var map = new google.maps.Map(document.getElementById('google-container'), map_options);
+//    //add a custom marker to the map				
+//    var marker = new google.maps.Marker({
+//        position: new google.maps.LatLng($latitude, $longitude),
+//        map: map,
+//        visible: true,
+//        icon: $marker_url,
+//    });
 
   
-});
+//});
 
 
 /*---------------------------------------*/
 /*	TIMELINE SLIDER
 	/*---------------------------------------*/
-jQuery(window).load(function() {
+//jQuery(window).load(function() {
 
-    'use strict';
+//    'use strict';
 
-    var x = 0,
-        init,
-        container = $('.timeline-section'),
-        /* TIMELINE SELECTOR */
-        items = container.find('li'),
-        containerHeight = 0,
-        numberVisible = 4,
-        /* NUMBER OF <li> TO SHOW IN SCROLLER */
-        intervalSec = 4000; /* INTERVAL TIME */
+//    var x = 0,
+//        init,
+//        container = $('.timeline-section'),
+//        /* TIMELINE SELECTOR */
+//        items = container.find('li'),
+//        containerHeight = 0,
+//        numberVisible = 4,
+//        /* NUMBER OF <li> TO SHOW IN SCROLLER */
+//        intervalSec = 4000; /* INTERVAL TIME */
 
-    if (!container.find('li:first').hasClass("first")) {
-        container.find('li:first').addClass("first");
-    }
+//    if (!container.find('li:first').hasClass("first")) {
+//        container.find('li:first').addClass("first");
+//    }
 
-    items.each(function() {
-        if (x < numberVisible) {
-            containerHeight = containerHeight + $(this).outerHeight();
-            x = x + 1;
-        }
-    });
+//    items.each(function() {
+//        if (x < numberVisible) {
+//            containerHeight = containerHeight + $(this).outerHeight();
+//            x = x + 1;
+//        }
+//    });
 
-    container.css({
-        height: containerHeight,
-        overflow: "hidden"
-    });
+//    container.css({
+//        height: containerHeight,
+//        overflow: "hidden"
+//    });
 
-    function vertCycle() {
-        var firstItem = container.find('li.first').html();
+//    function vertCycle() {
+//        var firstItem = container.find('li.first').html();
 
-        container.append('<li>' + firstItem + '</li>');
-        firstItem = '';
-        container.find('li.first').animate({
-            marginTop: "-105px",
-            opacity: "0"
-        }, 600, function() {
-            $(this).remove();
-            container.find('li:first').addClass("first");
-        });
-    }
+//        container.append('<li>' + firstItem + '</li>');
+//        firstItem = '';
+//        container.find('li.first').animate({
+//            marginTop: "-105px",
+//            opacity: "0"
+//        }, 600, function() {
+//            $(this).remove();
+//            container.find('li:first').addClass("first");
+//        });
+//    }
 
-    if (intervalSec < 700) {
-        intervalSec = 700;
-    }
+//    if (intervalSec < 700) {
+//        intervalSec = 700;
+//    }
 
-    init = setInterval(function() {
-        vertCycle();
-    }, intervalSec);
+//    init = setInterval(function() {
+//        vertCycle();
+//    }, intervalSec);
 
-    container.hover(function() {
-        clearInterval(init);
-    }, function() {
-        init = setInterval(function() {
-            vertCycle();
-        }, intervalSec);
-    });
-});
+//    container.hover(function() {
+//        clearInterval(init);
+//    }, function() {
+//        init = setInterval(function() {
+//            vertCycle();
+//        }, intervalSec);
+//    });
+//});
